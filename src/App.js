8f3ade230346e74a1b0377b20dc8fd0d1f1a7c1f
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-12-08 12:35:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,3 +14,147 @@ const App = () => {
 }
 
 export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Home from './Home'
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<Home/>}></Route>
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
